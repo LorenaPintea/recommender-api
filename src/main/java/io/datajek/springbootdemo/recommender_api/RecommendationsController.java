0e7c3eac7a284b5e8a52e9ff675219ee110ace59
Lorena Pintea <lorenamihaelapintea@gmail.com>
2024-06-12 21:24:59 +0100

    Creating a REST Service with Spring Boot - Done

--- src/main/java/io/datajek/springbootdemo/recommender_api/RecommendationsController.java
+++ src/main/java/io/datajek/springbootdemo/recommender_api/RecommendationsController.java
@@ -1,5 +1,6 @@
 package io.datajek.springbootdemo.recommender_api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,14 @@ import java.util.List;
 @RestController
 public class RecommendationsController {
     //method to return all movies
-    @RequestMapping(method = RequestMethod.GET, value = "/movies")
+    //@RequestMapping(method = RequestMethod.GET, value = "/movies")
+    @GetMapping("/movies")
     public List<Movie> getAllMovies() {
         return Arrays.asList(new Movie(1, "Ice Age", 7.5),
                 new Movie(2, "Happy Feet", 6.4),
-                new Movie(3, "Shark Tales", 6.0));
+                new Movie(3, "Shark Tales", 6.0),
+                new Movie(4, "Miami Bici 1", 5.6),
+                new Movie(5, "Miami Bici 2", 5.1),
+                new Movie(6, "Test DevTools", 9.5));
     }
 }
